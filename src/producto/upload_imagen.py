6c6c6be37324d84ef69aa303b817c405594b08b6
Elmer Villegas <elmer.villegas@utec.edu.pe>
2025-11-09 19:40:51 -0500

    feat: update 41

--- src/producto/upload_imagen.py
+++ src/producto/upload_imagen.py
@@ -8,7 +8,6 @@ def _resp(code, body):
 
 def lambda_handler(event, context):
     try:
-
         body = json.loads(event.get("body") or "{}")
         bucket = PRODUCTS_BUCKET
         key = body.get("key")
@@ -17,14 +16,14 @@ def lambda_handler(event, context):
         content_type = body.get("content_type")
 
         if not bucket:
-            return _resp(400, {"error":"Falta 'bucket'"})
-        
-        # Si no se proporciona el 'kfilenameey', usa el 'key' como el nombre del archivo
+            return _resp(400, {"error": "Falta 'bucket'"})
+
+        # Si no se proporciona el 'filename', usa el 'key' como el nombre del archivo
         if not filename:
-            key = filename
+            filename = key  # Asignar 'key' a 'filename' si 'filename' no está presente
 
         if not file_b64:
-            return _resp(400, {"error":"'file_base64' es requerido"})
+            return _resp(400, {"error": "'file_base64' es requerido"})
 
         try:
             file_bytes = base64.b64decode(file_b64)
@@ -32,7 +31,7 @@ def lambda_handler(event, context):
             return _resp(400, {"error": f"file_base64 inválido: {e}"})
 
         s3 = boto3.client("s3")
-        put_kwargs = {"Bucket": bucket, "Key": key, "Body": file_bytes}
+        put_kwargs = {"Bucket": bucket, "Key": filename, "Body": file_bytes}
         if content_type:
             put_kwargs["ContentType"] = content_type
 
@@ -40,7 +39,7 @@ def lambda_handler(event, context):
         etag = (resp.get("ETag") or "").strip('"')
 
         return _resp(200, {
-            "bucket": bucket, "key": key,
+            "bucket": bucket, "key": filename,  # Usa filename como 'key'
             "size_bytes": len(file_bytes), "etag": etag,
             "message": "Archivo subido correctamente."
         })
@@ -48,7 +47,7 @@ def lambda_handler(event, context):
     except ClientError as e:
         code = e.response.get("Error", {}).get("Code")
         if code == "AccessDenied":
-            return _resp(403, {"error":"Acceso denegado al bucket"})
+            return _resp(403, {"error": "Acceso denegado al bucket"})
         if code == "NoSuchBucket":
             return _resp(400, {"error": f"El bucket {bucket} no existe"})
         return _resp(400, {"error": f"Error S3: {e}"})
